962823f87ae3ead106df4a06b1f1a0eaf4b44b61
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-15 00:15:37 -0600

    added another dblclick listener - number 7

--- js/index.js
+++ js/index.js
@@ -21,12 +21,13 @@ dragOverP.forEach( p => {
     })
 });
 
-// #3 using click, click on buttons to console log action
+// #3 using click, click on buttons to console log action and change button green
 let buttons= document.querySelectorAll('.content-pick .btn'); //selecting buttons
 
 buttons.forEach(content => {
     content.addEventListener('click', function(event){
         console.log('You clicked a button');
+        event.target.style.background = "green";
         event.stopPropagation(); // stops any other events
     })
 })
@@ -42,7 +43,6 @@ images.forEach(content => {
 })
 
 // #5 using keydown, console log key pressed in form area
-
 let formArea = document.querySelector('.form-container'); // selecting form area
 
 formArea.addEventListener('keydown', function(event){
@@ -52,12 +52,23 @@ formArea.addEventListener('keydown', function(event){
 // #6 using click, when submit button is clicked, height and width of buttom changes to 200px
 let submitBtn = document.querySelector('.form-container .row-btn'); //selecting submit button
 
-console.log(submitBtn);
-
 submitBtn.addEventListener('click', function(event){
     event.preventDefault(); //stops button from refreshing
     console.log('You pressed the submit button!');
     event.target.style.height = "200px";
     event.target.style.width = "200px";
 });
+
+// #7 using dblclick, when h2s are double clicked, console log then starts back and forth animation
+let h2s = document.querySelectorAll('.container h2'); //selecting h2s
+
+h2s.forEach(content => {
+    content.addEventListener('dblclick', function(event){
+        console.log('You double clicked on a h2!');
+        event.target.style.animationDuration = "3s";
+        event.target.style.animationName = "slidein";
+        event.target.style.animationIterationCount = "infinite";
+        event.target.style.animationDirection = "alternate";
+    })
+})
 
