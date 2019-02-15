479267ba8ca58e866b98c8221a0e3f97a2b58623
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-14 23:05:57 -0600

    added dblclick listener

--- js/index.js
+++ js/index.js
@@ -1,30 +1,40 @@
 // Your code goes here
 
 // #1 using mouseover, hover over nav to change color to orange
-const navArea = document.querySelectorAll('.nav');
+const navArea = document.querySelectorAll('.nav'); // selecting nav area
 
 navArea.forEach(content => {
   content.addEventListener('mouseover', function(event){
-  console.log('mouseover');
+  console.log('You hovered over the nav');
   event.target.style.color = "orange"; })
 });
 
 // #2 using drag, drag anything over paragraphs to console log action
-const dragOverP = document.querySelectorAll('p');
+const dragOverP = document.querySelectorAll('p'); // selecting all p's
 
 dragOverP.forEach( p => {
     p.addEventListener('dragleave', function(){
-        console.log(`This object you are dragging is over a paragraph`);
+        console.log(`The object you are dragging is over a paragraph`);
         event.stopPropagation();
     })
 });
 
 // #3 using click, click on buttons to console log action
-let buttons= document.querySelectorAll('.content-pick .btn');
+let buttons= document.querySelectorAll('.content-pick .btn'); //selecting buttons
 
 buttons.forEach(content => {
     content.addEventListener('click', function(event){
-        console.log('Clicked Button');
+        console.log('You clicked a button');
         event.stopPropagation();
+    })
+})
+
+// #4 using dblclick, double cick an image to make the width to 50%
+let images = document.querySelectorAll('.container img'); //selecting images
+
+images.forEach(content => {
+    content.addEventListener('dblclick', function(event){
+        console.log('You double clicked a image');
+        event.target.style.width = "50%";
     })
 })
