3f167bc1693855d83605505e61dd6b693b3a89d8
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-14 22:07:32 -0600

    added 2 unique event listeners - mouseover and dragleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+// #1 using mouseover, hover over nav to change color to orange
+const navArea = document.querySelectorAll('.nav');
+
+navArea.forEach(content => {
+  content.addEventListener('mouseover', function(event){
+  console.log('mouseover');
+  event.target.style.color = "orange"; })
+});
+
+// #2 using drag, drag anything over 'p' to console log action
+const dragOverP = document.querySelectorAll('p');
+
+dragOverP.forEach( p => {
+    p.addEventListener('dragleave', function(){
+        console.log(`This object is over a paragraph`);
+    })
+});
