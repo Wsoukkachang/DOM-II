1d824c448be9e3541148b420d0ab99d9f479610a
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-16 13:31:40 -0600

    added mouseover listener - number 9 and 10

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// #1 using mouseover, hover over nav to change color to orange
+// #1 using mouseover, hover over nav to change color to orange and split
 const navArea = document.querySelectorAll('.nav a'); // selecting nav area
 
 navArea.forEach(content => {
@@ -60,7 +60,7 @@ let submitBtn = document.querySelector('.form-container .row-btn'); //selecting 
 
 submitBtn.addEventListener('click', function(event){
     event.preventDefault(); //stops button from refreshing
-    console.log('You pressed the submit button!');
+    console.log('You pressed the submit button');
     event.target.style.height = "200px";
     event.target.style.width = "200px";
 });
@@ -70,23 +70,45 @@ let h2s = document.querySelectorAll('.container h2'); //selecting h2s
 
 h2s.forEach(content => {
     content.addEventListener('dblclick', function(event){
-        console.log('You double clicked on a h2!');
+        console.log('You double clicked on a h2');
         event.target.style.animationDuration = "3s";
-        event.target.style.animationName = "slidein";
+        event.target.style.animationName = "slidein"; //makes sliding animation
         event.target.style.animationIterationCount = "infinite";
         event.target.style.animationDirection = "alternate";
     })
 })
 
-// #8 using dblclick, when h1 isdouble clicked, console log then starts animations
+// #8 using dblclick, when h1 is double clicked, console log then starts animations
 let h1main = document.querySelector('.main-navigation h1'); //selecting h1
 
 h1main.addEventListener('dblclick', function(event){
-    console.log('You double clicked on a h1!');
+    console.log('You double clicked on a h1');
     TweenMax.to(".logo-heading", 1, {opacity:0.6});
     TweenMax.to(".logo-heading", 3, {x:300});
-    TweenMax.to(".logo-heading", 3, {rotation:360, scale:1.4});
+    TweenMax.to(".logo-heading", 3, {rotation:1440, scale:1.4});
     TweenMax.to("html", 0.5, {"--myColor":"green", yoyo:true, repeat:-1}); //changes color from black to green-- repeats
-    TweenLite.to(h1main, 2, {rotationX:45, scaleX:0.8, z:-300});
+    TweenLite.to(h1main, 2, {rotationX:45, scaleX:0.8, z:-300}); //makes it look 3D
 })
 
+// #9 using mouseover, hover over footer "copyright text" to change text and start animation
+let copyrightText = document.querySelector('footer p'); //selecting footer p
+
+console.log(copyrightText);
+
+copyrightText.addEventListener('mouseover', function(event){
+    console.log('You hovered over a footer');
+    copyrightText.textContent = 'THIS SITE IS SO FUNNNNNN!!!'; //changes text
+    TweenMax.to("footer", 3, {rotation:720, scale:1.0}); //spins footer
+})
+
+// #10 using mouseover, double clicked over a destination content and start animation
+let destinations = document.querySelectorAll('.destination'); //selecting destination content
+
+destinations.forEach( element => {
+    element.addEventListener('dblclick', function(){
+        console.log(`You double clicked over a destination content`);
+        TweenMax.to(destinations[0], 3, {x:590, opacity:0.5}); // moves around content 
+        TweenMax.to(destinations[1], 3, {x:-250, opacity:0.5}); // moves around content 
+        TweenMax.to(destinations[2], 3, {x:-250, rotation:360, scale:1.1}); //moves around content and makes center one larger
+    })
+});
